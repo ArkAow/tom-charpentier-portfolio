170b4348f10f03c4e6911bae93d9646f28164ddd
ArkAow <tom.charpentier@outlook.fr>
2024-10-14 23:05:12 +0200

    Instanciating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 
 import Navbar from "./components/NavBar";
+import About from "./components/About";
 import City from "./components/City";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -9,8 +12,11 @@ const App = () => {
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
+          <About />
         </div>
         <City />
+        <Experience />
+        <Contact />
       </div>
     </BrowserRouter>
   );
