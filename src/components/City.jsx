78205cde37d6939870dff90584a2667bbf10121f
ArkAow <tom.charpentier@outlook.fr>
2024-10-15 23:14:15 +0200

    Changing City component Images

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -5,10 +5,9 @@ export default function City() {
         const handleScroll = () => {
           const scrollPosition = window.scrollY;
           
-          document.querySelector('.city-2').style.transform = `translateY(${scrollPosition * -0.2}px)`;
-          document.querySelector('.city-3').style.transform = `translateY(${scrollPosition * -0.2}px)`;
+          document.querySelector('.city-2').style.transform = `translateY(${scrollPosition * -0.8}px)`;
+          document.querySelector('.city-3').style.transform = `translateY(${scrollPosition * -0.8}px)`;
         };
-        
         window.addEventListener('scroll', handleScroll);
         
         return () => window.removeEventListener('scroll', handleScroll);
@@ -19,9 +18,10 @@ export default function City() {
          className="w-full">
             
           <div class="city-parallax-container relative">
-            <img src="/parallax/city-1.png" className="parallax-layer city-1" />
-            <img src="/parallax/city-2.png" className="parallax-layer city-2" />
-            <img src="/parallax/city-3.png" className="parallax-layer city-3 w-[40vw]" />
+            <img src="/parallax/city-ground.png" className="parallax-layer city-grd" />
+            <img src="/parallax/city-foreground.png" className="parallax-layer city-fg" />
+            <img src="/parallax/city-background.png" className="parallax-layer city-bg" />
+            <img src="/parallax/city-moon.png" className="parallax-layer city-moon w-[40vw]" />
           </div>
         </section>
     );
