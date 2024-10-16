043a1ad7e8e22745db626b0ac25a1d7eb87d54f8
Tom <tom.charpentier@etu.unilim.fr>
2024-10-16 11:40:53 +0200

    Added parallax to City component images

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -13,17 +13,51 @@ export default function City() {
         return () => window.removeEventListener('scroll', handleScroll);
         }, []);
 
+        useEffect(() => {
+          const handleParallax = (e) => {
+            const parallax = document.querySelector('.parallax');
+            const back = document.querySelector('.parallax-back-layer');
+            const front = document.querySelector('.parallax-front-layer');
+            const sFront = 300;
+            const sBack = 400;
+            const xOffest = -800
+      
+            if (parallax && front && back) {
+              const x = e.clientX + xOffest;
+              const y = e.clientY;
+      
+              back.style.transform = `translate(${x / sBack}%, ${y / sBack}%)`;
+              front.style.transform = `translate(${x / sFront}%, ${y / sFront}%)`;
+            }
+          };
+      
+          window.addEventListener('mousemove', handleParallax);
+      
+          return () => window.removeEventListener('mousemove', handleParallax);
+        }, []);
+
     return (
         <section id="city"
          className="w-full">
             
           <div className="city-parallax-container relative">
             <img src="/parallax/city-moon.png" className="parallax-layer city-moon w-[40vw]" />
+
+            <div className="parallax">
+
+              <div className="parallax-back-layer">
+                <img src="/parallax/city-foreground.png" className="parallax-layer city-fg" />
+              </div>           
+
+              <div className="parallax-front-layer">
+                <img src="/parallax/city-left-building.png" className="parallax-layer city-bld" />
+                <img src="/parallax/city-right-building.png" className="parallax-layer city-bld" />
+              </div>
+            </div>
+
             <img src="/parallax/city-ground.png" className="parallax-layer city-grd" />
             <img src="/parallax/city-background.png" className="parallax-layer city-bg" />
-            <img src="/parallax/city-foreground.png" className="parallax-layer city-fg" />
-            <img src="/parallax/city-left-building.png" className="parallax-layer city-bld" />
-            <img src="/parallax/city-right-building.png" className="parallax-layer city-bld" />
+            
           </div>
         </section>
     );
