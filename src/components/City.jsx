e6f6dfb6fca98e629f05dcbb9391c4fda3c18d36
ArkAow <tom.charpentier@outlook.fr>
2024-10-18 07:18:33 +0200

    adding more layers to parallax

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -15,17 +15,19 @@ export default function City() {
     useEffect(() => {
       const handleParallax = (e) => {
         const parallax = document.querySelector('.parallax');
-        const back = document.querySelector('.parallax-back-layer');
+        const back1 = document.querySelector('.parallax-back-1-layer');
+        const back2 = document.querySelector('.parallax-back-2-layer');
         const front = document.querySelector('.parallax-front-layer');
         const sFront = 1000;
-        const sBack = 2500;
+        const sBack1 = 3000;
+        const sBack2 = 2000;
         const xOffest = -2;
 
-        if (parallax && front && back) {
+        if (parallax && front && back1 && back2) {
           const x = e.clientX;
-
-          back.style.transform = `translateX(${(x / sBack) + xOffest}%)`;
-          front.style.transform = `translateX(${(x / sFront) + xOffest}%)`;
+          back1.style.transform = `translate(${(x / sBack1) + xOffest}%)`;
+          back2.style.transform = `translate(${(x / sBack2) + xOffest}%)`;
+          front.style.transform = `translate(${(x / sFront) + xOffest}%)`;
         }
       };
       window.addEventListener('mousemove', handleParallax);
@@ -42,13 +44,18 @@ export default function City() {
             <div className="parallax">
 
               <div className="parallax-bg-layer">
-                <img src="/parallax/city-background.png" className="parallax-layer city-bg" />
+              <img src="/parallax/city-background-1.png" className="parallax-layer city-bg" />
+              <img src="/parallax/city-background-2.png" className="parallax-layer city-bg" />
                 <img src="/parallax/city-ground.png" className="parallax-layer city-grd" />                
               </div>
 
-              <div className="parallax-back-layer">
-                <img src="/parallax/city-foreground.png" className="parallax-layer city-fg" />
-              </div>           
+              <div className="parallax-back-1-layer">
+                <img src="/parallax/city-foreground-1.png" className="parallax-layer city-fg" />
+              </div>
+
+              <div className="parallax-back-2-layer">
+                <img src="/parallax/city-foreground-2.png" className="parallax-layer city-fg" />
+              </div>         
 
               <div className="parallax-front-layer">
                 <img src="/parallax/city-left-building.png" className="parallax-layer city-buildings" />
