7c2e05bc90786c5b551017684e449db2a29be2f6
ArkAow <tom.charpentier@outlook.fr>
2024-10-20 17:55:36 +0200

    Resprite Background-1

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Experience() {
     return (
-        <div className="w-full flex flex-col items-center justify-center font-bold mt-16 p-6">
+        <div className="w-full flex flex-col items-left ml-12 justify-left font-bold mt-16 p-6">
             <h1 className="text-4xl relative">Professianal Experience</h1>
         </div>
     );
