9aeadfdf998bb1b0e8695cd53af8c47e22dcaaf5
Tom <tom.charpentier@etu.unilim.fr>
2024-10-28 22:29:03 +0100

    Added temporary text to components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,8 @@ export default function Contact() {
     return (
         <div className="w-full flex flex-col items-center justify-center font-bold mt-16 p-6">
             <h1 className="text-4xl relative">Contact Me</h1>
+            <p>email sender</p>
+            <p>other links</p>
         </div>
     );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,6 +4,10 @@ export default function Experience() {
     return (
         <div className="w-full flex flex-col items-left ml-12 justify-left font-bold mt-16 p-6">
             <h1 className="text-4xl relative">Professianal Experience</h1>
+            <p>some text</p>
+            <p>languages, frameworks and tools caroussel</p>
+            <p>compagnies i've worked for animated cards</p>
+            
         </div>
     );
 }
