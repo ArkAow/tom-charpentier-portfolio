24cabff8baa21be3567f53ef240df150dd42bf64
Tom <tom.charpentier@etu.unilim.fr>
2024-10-30 16:00:00 +0100

    Added some text to experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,10 +5,20 @@ export default function Experience() {
     return (
         <div id="experience"
           className="w-screen flex flex-col items-left justify-left mt-16 p-10">
-            <h1 className="text-4xl relative font-bold">My Experience</h1>
-            <p>some text</p>
+            <h1 className="text-4xl relative font-bold pb-12">My Experience</h1>
+            <p className="text-justify">
+                Throughout my studies, I have gained extensive experience in various programming languages and development tools. 
+                My academic journey has allowed me to acquire strong foundational skills in software development, with a focus on 
+                three key areas: software development, mobile application development, and web development.
+                <br/><br/>
+                This versatile skill set enables me to adapt to diverse project requirements and to collaborate effectively within 
+                multidisciplinary teams. I am committed to continuous learning and staying updated with the latest advancements in 
+                technology to provide innovative solutions and deliver high-quality work.
+                <br/>
+                Here are some technologies I have worked with:
+            </p>
             <LogoCarousel/>
-            <p>compagnies i've worked for animated cards</p>
+            <p>compagnies i've worked for -- animated cards</p>
         </div>
     );
 }
