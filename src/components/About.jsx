7a2436e0e50c93fe082dc1aeaae31a25e8435a82
Tom <tom.charpentier@etu.unilim.fr>
2024-10-30 13:30:39 +0100

    Added illustrations to about componenent

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
 
     return (
         <div id="about"
-          className="w-full flex flex-col items-center">
+          className="w-full flex flex-col items-center mt-16 p-10">
             <h1 className="font-bold text-4xl relative"> About
                 <span className="relative inline-block ml-2"> Me
                     <span className={`h-1 border-t-4 border-white-100 rounded-xl mt-1 top-full transition-all duration-500 ${animate ? 'w-0 absolute right-0' : 'w-full absolute left-0'}`}></span>
@@ -25,24 +25,38 @@ export default function About() {
                 <div className="mt-8 flex flex-col w-3/4 items-left relative">
                     <p className="mb-2">
                         Hi, I am Tom Charpentier.
-                        I am a third year computer science student at the <em>"IUT du Limousin"</em>.
+                        I am a third year computer science student at the <em>IUT du Limousin</em>.
                     </p>
                     <p className="mb-2">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula ex tortor, sed blandit risus facilisis quis. Nulla fermentum orci eget porta molestie. Aliquam erat volutpat. Curabitur et nibh eget nunc sodales eleifend eget nec lectus. Fusce et diam ac quam eleifend porttitor in a tortor. Ut sit amet lacinia quam. Curabitur et malesuada orci, at dapibus sem. Aliquam fermentum leo enim, sit amet lobortis quam viverra in. Donec sed mauris turpis. Suspendisse gravida odio tempus, tempor magna sed, malesuada diam. Phasellus pretium odio at justo dictum, at rhoncus eros tempor. Ut viverra at felis ut mattis.
                     </p>
-                    <p className="mb-2">
-                    Vivamus varius auctor nulla, non semper sem pharetra vitae. Sed eu varius arcu. Proin molestie sem sed purus ornare cursus. Aliquam quis tellus et diam pretium varius. Etiam id ipsum tincidunt enim varius ultricies ut vitae erat. Proin ut lobortis mi, vel placerat diam. Nullam ut cursus eros. Fusce dignissim condimentum tincidunt. Suspendisse tincidunt tempor velit ac malesuada. Nunc auctor turpis at gravida interdum.
-                    </p>
+                    <div className="relative z-10">
+                        <p className="mb-2 relative">
+                            Vivamus varius auctor nulla, non semper sem pharetra vitae. Sed eu varius arcu. Proin molestie sem sed purus ornare cursus. Aliquam quis tellus et diam pretium varius. Etiam id ipsum tincidunt enim varius ultricies ut vitae erat. Proin ut lobortis mi, vel placerat diam. Nullam ut cursus eros. Fusce dignissim condimentum tincidunt. Suspendisse tincidunt tempor velit ac malesuada. Nunc auctor turpis at gravida interdum.
+                        </p>
+                    </div>
+
+                    <img 
+                        src="/icons/gaming.svg"
+                        className={`stroke-slate-300 w-16 absolute -rotate-12 transition-all duration-[500ms] z-0 ${animate ? 'bottom-10' : 'bottom-16'}`}/>
+                    <img 
+                        src="/icons/piano.svg"
+                        className={`stroke-slate-300 w-16 absolute -rotate-6 left-20 transition-all duration-[1000ms] z-0 ${animate ? 'bottom-10' : 'bottom-20'}`}/>
+                    <img 
+                        src="/icons/cooking.svg"
+                        className={`stroke-slate-300 w-16 absolute rotate-6 left-40 transition-all duration-[1000ms] z-0 ${animate ? 'bottom-10' : 'bottom-20'}`}/>
+                    <img 
+                        src="/icons/pen.svg"
+                        className={`stroke-slate-300 w-16 absolute rotate-12 left-60 transition-all duration-[500ms] z-0 ${animate ? 'bottom-10' : 'bottom-16'}`}/>   
                 </div> 
                 <div className="items-end justify-end relative ml-[5%]">
                     <img 
                         src="/res/about-photo.png" 
                         className="absolute top-5 left-20 hidden md:flex w-[15vw] transition-transform duration-300 hover:scale-125 z-10"
-                    />
+                        alt="photo of me :)"/>
                     <img 
                         src="/res/about-photo-back-image.png" 
-                        className="hidden md:flex w-[40vw]"
-                    />
+                        className="hidden md:flex w-[40vw]"/>
                 </div>
             </div>
             
