6ab6b02e03a89a72ad312a321dcc16face7fe7d9
Tom <tom.charpentier@etu.unilim.fr>
2024-10-30 18:47:22 +0100

    Added text when hovering logo in Experience component

--- src/components/LogoCaroussel.jsx
+++ src/components/LogoCaroussel.jsx
@@ -5,15 +5,18 @@ export default function LogoCarousel() {
   return (
     <div className="overflow-hidden relative w-full py-10">
       <div
-        className="slide-track flex w-[calc(150px*40)] animate-scroll group-hover:animate-none whitespace-nowrap"
-        style={{ animationDuration: "60s" }}
+        className="slide-track flex w-[calc(150px*40)] animate-scroll whitespace-nowrap"
+        style={{ animationDuration: "30s" }}
       >
         {[...logos, ...logos].map((logo, index) => (
-          <div key={index} className="flex items-center justify-center w-64 h-16">
+          <div key={index} className="group relative flex items-center justify-center w-64 h-16">
+            <span className=" font-bold absolute -top-6 text-sm text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              {logo.name}
+            </span>
             <img
               src={logo.url}
               alt={logo.name}
-              className="w-16 h-16 object-contain grayscale hover:grayscale-0 transition duration-300 hover:scale-110 hover:animate-none"
+              className="w-16 h-16 object-contain grayscale hover:grayscale-0 transition duration-300 group-hover:scale-110"
             />
           </div>
         ))}
