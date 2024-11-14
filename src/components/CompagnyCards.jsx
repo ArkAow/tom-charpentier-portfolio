3ff6b61758704ad8cfa6b1d4f5883152fb1fc7f8
ArkAow <tom.charpentier@outlook.fr>
2024-11-14 20:13:22 +0100

    Changed CompagnyCards on mobile

--- src/components/CompagnyCards.jsx
+++ src/components/CompagnyCards.jsx
@@ -6,8 +6,8 @@ export default function CompanyCards() {
     <div className="pt-10 flex flex-wrap gap-5 justify-start lg:justify-center w-full">
 
       {/* L'université du Limousin*/}
-      <button className="w-1/2 group relative h-24 rounded-lg border-2 border-primary bg-tertiary
-            overflow-hidden transition-grow duration-300 sm:focus:grow sm:focus:h-60 hover:border-[rgba(249,249,249,0.7)]">
+      <button className="min-w-40 w-1/2 group relative h-24 rounded-lg border-2 border-primary bg-tertiary
+            overflow-hidden transition-grow duration-300 focus:grow sm:focus:h-60 hover:border-[rgba(249,249,249,0.7)]">
         
         <img
           className="absolute left-9 sm:left-11 top-1/2 -translate-y-1/2 w-16 h-16 z-10 object-contain ease-in-out"
@@ -19,7 +19,7 @@ export default function CompanyCards() {
           src="res/iut-bg.png"
           alt="background"/>
 
-        <div className="opacity-0 z-10 sm:group-focus:opacity-100 absolute left-36 top-1/2 transform -translate-y-1/2 text-white text-sm font-medium">
+        <div className="absolute opacity-0 z-10 sm:group-focus:opacity-100 left-36 top-1/2 transform -translate-y-1/2 text-white text-sm font-medium">
             <p className="text-justify w-full pr-5">
                 During my studies, I worked on various group projects, known as SAEs (Situations d’Apprentissage et d’Évaluation), included by my school curriculum.
                 <br/>
@@ -32,11 +32,16 @@ export default function CompanyCards() {
                 iut.unilim.fr
             </a >
         </div>
+        <div className="absolute opacity-0 z-10 group-focus:opacity-100 sm:group-focus:opacity-0 left-36 top-1/2 transform -translate-y-1/2 text-white text-sm font-medium">
+            <p className="w-full justify-center items-center pr-5">
+                Université de Limoges (France)
+            </p>
+        </div>
       </button >
 
       {/* Biotonix */}
       <button className="min-w-40 w-1/2 group relative h-24 rounded-lg border-2 border-primary bg-tertiary
-            overflow-hidden transition-grow duration-300 sm:focus:grow sm:focus:h-60 hover:border-[rgba(249,249,249,0.7)]">
+            overflow-hidden transition-grow duration-300 focus:grow sm:focus:h-60 hover:border-[rgba(249,249,249,0.7)]">
         
         <img
           className="absolute left-1 sm:left-3 top-1/2 -translate-y-1/2 w-32 h-32 z-10 object-contain ease-in-out"
@@ -48,7 +53,7 @@ export default function CompanyCards() {
           src="res/biotonix-bg.png"
           alt="background"/>
 
-        <div className="opacity-0 z-10 sm:group-focus:opacity-100 absolute left-40 top-1/2 transform -translate-y-1/2 text-black text-sm font-medium">
+        <div className="absolute opacity-0 z-10 sm:group-focus:opacity-100 left-40 top-1/2 transform -translate-y-1/2 text-black text-sm font-medium">
             <p className="text-justify w-full pr-5">
                 During my second year in computer science, I had the opportunity to intern in Canada, thanks to a partnership between my IUT and the CEGEP of Trois-Rivières, near Montreal.
                 <br/>
@@ -62,12 +67,17 @@ export default function CompanyCards() {
                 biotonix.com
               </a >
         </div>
+        <div className="absolute opacity-0 z-10 group-focus:opacity-100 sm:group-focus:opacity-0 left-36 top-1/2 transform -translate-y-1/2 text-blue-600 text-sm font-medium">
+            <p className="w-full justify-center items-center pr-5">
+                Biotonix (Canada)
+            </p>
+        </div>
       </button >
 
       <hr className="w-1/2 border-t-4 border-accent rounded-xl"/>
       
       {/* En recherche */}
-      <div className=" min-w-40 w-1/2 group relative h-24 z-10 rounded-lg border-2 border-primary bg-tertiary
+      <div className="min-w-40 w-full sm:w-1/2 group relative h-24 z-10 rounded-lg border-2 border-primary bg-tertiary
             overflow-hidden transition-grow duration-300 hover:border-[rgba(249,249,249,0.7)]">
         
         <div className="z-10 absolute left-5 top-1/2 transform -translate-y-1/2 text-white text-sm font-bold">
