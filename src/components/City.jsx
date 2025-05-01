112f89e07ffda659a058d996bbd42fb3b4f24e2f
ArkAow <tom.charpentier@outlook.fr>
2025-05-01 13:30:23 +0200

    Added new images for background

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -46,23 +46,10 @@ export default function City() {
             <div className="parallax">
 
               <div className="parallax-bg-layer">
-                <img src="parallax/city-background-1.png" className="parallax-layer city-bg-1" />
-                <img src="parallax/city-background-2.png" className="parallax-layer city-bg-2" />
-                <img src="parallax/city-background-3.png" className="parallax-layer city-bg-3" />
+                <img src="parallax/city-background-12.png" className="parallax-layer city-bg-1" />
+                <img src="parallax/city-background-22.png" className="parallax-layer city-bg-2" />
+                <img src="parallax/city-background-32.png" className="parallax-layer city-bg-3" />
                 <img src="parallax/city-ground.png" className="parallax-layer city-grd translate-y-5" />                
-              </div>
-
-              <div className="parallax-back-1-layer">
-                <img src="parallax/city-foreground-1.png" className="parallax-layer city-fg" />
-              </div>
-
-              <div className="parallax-back-2-layer">
-                <img src="parallax/city-foreground-2.png" className="parallax-layer city-fg" />
-              </div>         
-
-              <div className="parallax-front-layer">
-                <img src="parallax/city-left-building.png" className="parallax-layer city-buildings" />
-                <img src="parallax/city-right-building.png" className="parallax-layer city-buildings" />
               </div>
             </div>           
           </div>
