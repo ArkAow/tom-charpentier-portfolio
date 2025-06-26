0fa27fbb7c1e4223855614bfb63fff228e369a88
ArkAow <tom.charpentier@outlook.fr>
2025-06-26 16:51:46 +0200

    Cleaned Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,11 @@ const Footer = () => {
     <footer className="w-full bg-primary text-secondary py-6 border-t border-gray-700 mt-8">
       <div className="container mx-auto flex flex-col items-center gap-6 px-4">
         
-        {/* Liens avec icônes */}
         <ul className="flex flex-row gap-8 justify-center items-center">
           {redirectLinks.map((link) => (
             <li
               key={link.id}
-              className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer flex items-center gap-2 transition duration-300"
-            >
+              className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer flex items-center gap-2 transition duration-300">
               <img
                 src={link.image_path}
                 alt="logo"
@@ -25,7 +23,6 @@ const Footer = () => {
           ))}
         </ul>
 
-        {/* Copyright */}
         <p className="text-xs md:text-sm text-center text-secondary opacity-75">
           ©2025 Tom Charpentier
         </p>
